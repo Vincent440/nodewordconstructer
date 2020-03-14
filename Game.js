afb02c8c49d61989bbf003553ba5a5d5822d1baa
Vincent440 <vinceshury@gmail.com>
2020-03-14 17:07:00 -0400

    refactored constructor functions to classes

--- Game.js
+++ Game.js
@@ -2,68 +2,57 @@ const Word = require('./Word.js')
 const inquirer = require('inquirer')
 const wordToBeGuessed = new Word('light-year')
 
-const Game = function () {
-  const self = this
-
-  self.guessesMade = []
-
-  self.numberOfGuessesAvailable = 10
-
-  self.currentWord = wordToBeGuessed
-
-  self.numberOfRemainingLettersToBeGuessed = self.currentWord.length
-
-  self.start = function () {
-    self.updateGame()
-  }
-  self.updateGame = function () {
-    self.numberOfRemainingLettersToBeGuessed = 0
-
-    self.currentWord.letters.forEach(letter => {
-      if (!letter.guessed) {
-        self.numberOfRemainingLettersToBeGuessed++
-      }
-      if (self.numberOfRemainingLettersToBeGuessed > self.numberOfGuessesAvailable) {
-        self.gameOver()
-      }
-    })
-    self.promptUserGuess()
-  }
-
-  self.promptUserGuess = function () {
-    console.log(self.currentWord.display())
-    inquirer.prompt([
-      {
-        name: 'guess',
-        type: 'input',
-        message: 'Guess a letter!\n',
-        validate: function (char) {
-          const validLetterInput = new RegExp(/[a-z]+/g)
-          if (char.length === 1 && validLetterInput.test(char)) {
-            return true
-          }
-          return 'Must guess a single letter A - Z'
-        }
-      }]).then(userPick => {
-      console.log(self.numberOfGuessesAvailable)
-      console.log(self.numberOfRemainingLettersToBeGuessed)
-      self.guessesMade.push(userPick.guess)
-      self.currentWord.guessLetter(userPick.guess)
-      console.log(self.guessesMade)
-      self.next()
-    })
-  }
-
-  self.next = function () {
-    self.updateGame()
-    if (self.numberOfGuessesAvailable === 0) {
-      self.gameOver()
+class Game {
+  constructor () {
+    this.guessesMade = []
+    this.numberOfGuessesAvailable = 10
+    this.currentWord = wordToBeGuessed
+    this.numberOfRemainingLettersToBeGuessed = this.currentWord.length
+    this.start = function () {
+      this.updateGame()
     }
-  }
-
-  self.gameOver = function () {
-    console.log('Good game')
-    process.exit(1)
+    this.updateGame = function () {
+      this.numberOfRemainingLettersToBeGuessed = 0
+      this.currentWord.letters.forEach(letter => {
+        if (!letter.guessed) {
+          this.numberOfRemainingLettersToBeGuessed++
+        }
+        if (this.numberOfRemainingLettersToBeGuessed > this.numberOfGuessesAvailable) {
+          this.gameOver()
+        }
+      })
+      this.promptUserGuess()
+    }
+    this.promptUserGuess = function () {
+      console.log(this.currentWord.display())
+      inquirer.prompt([
+        {
+          name: 'guess',
+          type: 'input',
+          message: 'Guess a letter!\n',
+          validate: function (char) {
+            const validLetterInput = new RegExp(/[a-z]+/g)
+            if (char.length === 1 && validLetterInput.test(char)) {
+              return true
+            }
+            return 'Must guess a single letter A - Z'
+          }
+        }
+      ]).then(userPick => {
+        this.guessesMade.push(userPick.guess)
+        this.currentWord.guessLetter(userPick.guess)
+        this.next()
+      })
+    }
+    this.next = function () {
+      this.updateGame()
+      if (this.numberOfGuessesAvailable === 0) {
+        this.gameOver()
+      }
+    }
+    this.gameOver = function () {
+      process.exit(1)
+    }
   }
 }
 
--- Letter.js
+++ Letter.js
@@ -1,36 +1,29 @@
-const Letter = function (singleCharacter) {
-  // A string value to store the underlying character for the letter
-  switch (singleCharacter.length) {
-    case 1:
-      this.character = singleCharacter
-      break
-    default:
-      throw new Error('A letter can ONLY be a SINGLE character')
-  }
-
-  // A boolean value that stores whether that letter has been guessed yet
-  if (this.character === '-' || this.character === ' ') {
-    this.guessed = true
-  } else if (this.character === '_') {
-    throw new Error('Underscores are Reserved characters')
-  } else {
-    this.guessed = false
-  }
-
-  // Letter's display function is named toString,
-  this.toString = function () {
-    // JavaScript will call that function automatically whenever casting that object to a string
-    // A function that returns the underlying character if the letter has been guessed,
-    return this.guessed ? this.character : '_'
-  }
-
-  // A function that takes a character as an argument and checks it against the underlying character,
-  this.makeGuess = function (guessedChar) {
-    if (guessedChar.toLowerCase() === this.character.toLowerCase()) {
-      this.guessed = true// updating the stored boolean value to true if it was guessed correctly
-      return true
+class Letter {
+  constructor (singleCharacter) {
+    switch (singleCharacter.length) {
+      case 1:
+        this.character = singleCharacter
+        break
+      default:
+        throw new Error('A letter can ONLY be a SINGLE character')
+    }
+    if (this.character === '-' || this.character === ' ') {
+      this.guessed = true
+    } else if (this.character === '_') {
+      throw new Error('Underscores are Reserved characters')
     } else {
-      return false
+      this.guessed = false
+    }
+    this.toString = function () {
+      return this.guessed ? this.character : '_'
+    }
+    this.makeGuess = function (guessedChar) {
+      if (guessedChar.toLowerCase() === this.character.toLowerCase()) {
+        this.guessed = true
+        return true
+      } else {
+        return false
+      }
     }
   }
 }
