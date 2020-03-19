5a53f973c75218c4555d75775487f165e9ce2670
vince s <vinceshury@gmail.com>
2020-03-18 21:57:02 -0400

    Word.js refactored to class, creating game object

--- Game.js
+++ Game.js
@@ -1,55 +1,59 @@
 const Word = require('./Word.js')
 const inquirer = require('inquirer')
 const wordToBeGuessed = new Word('light-year')
+const questionPrompt = {
+  name: 'guess',
+  type: 'input',
+  message: 'Guess a letter!\n',
+  validate: function (char) {
+    const validLetters = /[a-z]/g
+    if (char.length === 1 && char.test(validLetters)) {
+      return true
+    }
+    char = ''
+    return 'Must guess a single letter A - Z'
+  }
+}
 
 class Game {
   constructor () {
-    this.guessesMade = []
     this.numberOfGuessesAvailable = 10
     this.currentWord = wordToBeGuessed
     this.numberOfRemainingLettersToBeGuessed = this.currentWord.length
+
     this.start = function () {
-      this.updateGame()
+      this.update()
     }
-    this.updateGame = function () {
+
+    this.update = function () {
       this.numberOfRemainingLettersToBeGuessed = 0
       this.currentWord.letters.forEach(letter => {
         if (!letter.guessed) {
           this.numberOfRemainingLettersToBeGuessed++
         }
-        if (this.numberOfRemainingLettersToBeGuessed > this.numberOfGuessesAvailable) {
-          this.gameOver()
-        }
       })
-      this.promptUserGuess()
+      this.next()
     }
-    this.promptUserGuess = function () {
-      console.log(this.currentWord.display())
-      inquirer.prompt([
-        {
-          name: 'guess',
-          type: 'input',
-          message: 'Guess a letter!\n',
-          validate: function (char) {
-            const validLetterInput = new RegExp(/[a-z]+/g)
-            if (char.length === 1 && validLetterInput.test(char)) {
-              return true
-            }
-            return 'Must guess a single letter A - Z'
-          }
-        }
-      ]).then(userPick => {
-        this.guessesMade.push(userPick.guess)
-        this.currentWord.guessLetter(userPick.guess)
-        this.next()
-      })
-    }
+
     this.next = function () {
-      this.updateGame()
+      if (this.numberOfRemainingLettersToBeGuessed > this.numberOfGuessesAvailable) {
+        this.gameOver()
+      }
       if (this.numberOfGuessesAvailable === 0) {
         this.gameOver()
       }
+      this.promptUser()
     }
+
+    this.promptUser = function () {
+      console.log(this.currentWord.display())
+      inquirer.prompt([questionPrompt]).then(userPick => {
+        console.log(userPick)
+        this.currentWord.guessLetter(userPick.guess)
+        this.update()
+      })
+    }
+
     this.gameOver = function () {
       process.exit(1)
     }
--- Word.js
+++ Word.js
@@ -1,15 +1,14 @@
 const Letter = require('./Letter.js')
 
-const Word = function (word) {
-  // An array of new Letter objects representing the letters of the underlying word
-  this.letters = word.split('').map(char => new Letter(char))
-  // function that returns a string representing the word. This should call the function on each letter object
-  this.display = function () {
-    return this.letters.join('')
-  }
-  // A function that takes a character as an argument and calls the guess function on each letter object
-  this.guessLetter = function (char) {
-    return this.letters.forEach(letter => !!letter.makeGuess(char))
+class Word {
+  constructor (word) {
+    this.letters = word.split('').map(char => new Letter(char))
+    this.display = function () {
+      return this.letters.join('')
+    }
+    this.guessLetter = function (char) {
+      return this.letters.forEach(letter => !!letter.makeGuess(char))
+    }
   }
 }
 
